3c62492601aa7e20de28d47e206045b57ac6a31d
James Abney <james.abney@gmail.com>
2017-12-10 21:34:17 -0800

    Add to project

---
+++ test/jade.spec.js
@@ -0,0 +1,19 @@
+import compiler from './compiler.js'
+import helpers from './helpers'
+
+test('Renders a Jade template', async () => {
+  const options = {
+    engine: 'jade',
+    locals: {
+      title: 'Jade Template',
+      desc: 'A template rendered by Jade'
+    }
+  }
+
+  const stats = await compiler('data/source.jade', options)
+  const output = stats.toJson().modules[0].source
+  const source = helpers.value(output)
+
+  expect(source).toContain('<h1>Jade Template</h1>')
+  expect(source).toContain('<h2>A template rendered by Jade</h2>')
+})

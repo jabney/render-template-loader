d4b9bd5f6e1c2fc78a373330aaa6120c58315501
James Abney <james.abney@gmail.com>
2017-12-10 22:07:29 -0800

    Add custom template render spec

--- test/loader.spec.js
+++ test/loader.spec.js
@@ -1,55 +1,23 @@
-// import compiler from './compiler.js'
-// import helpers from './helpers'
+import compiler from './compiler.js'
+import helpers from './helpers'
 
-// test('Replaces template strings with values', async () => {
-//   const options = {
-//     regex: /#\{(.+?)\}/g,
-//     value: (match) => {
-//       const context = {
-//         date: new Date().toDateString(),
-//         time: new Date().toTimeString()
-//       }
-//       return context[match[1]]
-//     }
-//   }
+test('Renders a custom template', async () => {
+  const options = {
+    engine: function (str, locals) {
+      return str.replace(/#\{(.+?)\}/g, function (match, p1) {
+        return locals[p1]
+      })
+    },
+    locals: {
+      title: 'Custom Template',
+      desc: 'A template rendered by a custom function'
+    }
+  }
 
-//   const stats = await compiler('data/template.txt', options)
-//   const output = stats.toJson().modules[0].source
-//   const source = helpers.value(output)
+  const stats = await compiler('data/custom.txt', options)
+  const output = stats.toJson().modules[0].source
+  const source = helpers.value(output)
 
-//   expect(source).toMatch(/Today\'s date is \w{3} \w{3} \d{2} \d{4}/)
-//   expect(source).toMatch(/The time is \d{2}:\d{2}:\d{2} GMT(?:\+|-)\d{4} \(\w{3}\)/)
-// })
-
-// test('Replaces template strings with values in stages', async () => {
-//   const options = {
-//     stages: [{
-//       regex: /#\{(.+?)\}/g,
-//       value: (match) => {
-//         const vars = ['', 'glitter', 'lost', 'wither', 'frost']
-//         return vars[match[1]] || match[0]
-//       }
-//     },
-//     {
-//       regex: /#\{(.+?)\}/g,
-//       value: (match) => {
-//         const vars = ['', '', '', '', '', 'woken', 'spring', 'broken', 'king']
-//         return vars[match[1]] || match[0]
-//       }
-//     }]
-//   }
-
-//   const stats = await compiler('data/return.txt', options)
-//   const output = stats.toJson().modules[0].source
-//   const source = helpers.value(output)
-
-//   expect(source).toContain('All that is gold does not glitter,')
-//   expect(source).toContain('Not all those who wander are lost;')
-//   expect(source).toContain('The old that is strong does not wither,')
-//   expect(source).toContain('Deep roots are not reached by the frost.')
-//   expect(source).toContain('From the ashes a fire shall be woken,')
-//   expect(source).toContain('A light from the shadows shall spring;')
-//   expect(source).toContain('Renewed shall be blade that was broken,')
-//   expect(source).toContain('The crownless again shall be king.')
-//   expect(source).not.toMatch(/#\{.+?\}/)
-// })
+  expect(source).toContain('<h1>Custom Template</h1>')
+  expect(source).toContain('<h2>A template rendered by a custom function</h2>')
+})

6fdab8db66d627276157f7c78c5545edce8d8a76
James Abney <james.abney@gmail.com>
2017-12-10 19:52:11 -0800

    Add template rules

--- dev/webpack.config.js
+++ dev/webpack.config.js
@@ -1,3 +1,4 @@
+var ejs = require('ejs')
 
 module.exports = {
   entry: './dev/index.js',
@@ -6,11 +7,56 @@ module.exports = {
   },
   module: {
     rules: [{
-      test: /\.svg$/,
+      test: /\/index.ejs$/,
       use: {
         loader: './index.js',
         options: {
-
+          engine: function (str, locals, options) {
+            return ejs.render(str, locals, options)
+          },
+          engineOptions: {},
+          locals: {
+            title: 'Multi Template Loader'
+          }
+        }
+      }
+    },
+    {
+      test: /\/index.pug$/,
+      use: {
+        loader: './index.js',
+        options: {
+          engine: 'pug',
+          engineOptions: {},
+          locals: {
+            title: 'Multi Template Loader'
+          }
+        }
+      }
+    },
+    {
+      test: /\/index.mustache$/,
+      use: {
+        loader: './index.js',
+        options: {
+          engine: 'mustache',
+          engineOptions: {},
+          locals: {
+            title: 'Multi Template Loader'
+          }
+        }
+      }
+    },
+    {
+      test: /\/index.hbs$/,
+      use: {
+        loader: './index.js',
+        options: {
+          engine: 'handlebars',
+          engineOptions: {},
+          locals: {
+            title: 'Multi Template Loader'
+          }
         }
       }
     }]

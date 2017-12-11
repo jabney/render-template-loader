81b64d497309574dd2dcc24799a07fed4a691f33
James Abney <james.abney@gmail.com>
2017-12-11 11:10:39 -0800

    Add spec for pug partials

--- test/pug.spec.js
+++ test/pug.spec.js
@@ -1,5 +1,6 @@
 import compiler from './compiler.js'
 import helpers from './helpers'
+import path from 'path'
 
 test('Renders a Pug template', async () => {
   const options = {
@@ -11,6 +12,26 @@ test('Renders a Pug template', async () => {
   }
 
   const stats = await compiler('data/source.pug', options)
+  const output = stats.toJson().modules[0].source
+  const source = helpers.value(output)
+
+  expect(source).toContain('<h1>Pug Template</h1>')
+  expect(source).toContain('<h2>A template rendered by Pug</h2>')
+})
+
+test('Renders a Pug template with partial', async () => {
+  const options = {
+    engine: 'pug',
+    locals: {
+      title: 'Pug Template',
+      desc: 'A template rendered by Pug'
+    },
+    engineOptions: {
+      filename: path.resolve(__dirname, 'source-partial.pug')
+    }
+  }
+
+  const stats = await compiler('data/source-partial.pug', options)
   const output = stats.toJson().modules[0].source
   const source = helpers.value(output)
 

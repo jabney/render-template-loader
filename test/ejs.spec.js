71cf5779c1417cc223e3e18129ae08dfe95b6123
James Abney <james.abney@gmail.com>
2017-12-12 22:17:51 -0800

    Add tests for both views and filename engineOptions

--- test/ejs.spec.js
+++ test/ejs.spec.js
@@ -8,7 +8,8 @@ test('Renders an Ejs template', async () => {
     locals: {
       title: 'Ejs Template',
       desc: 'A template rendered by Ejs'
-    }
+    },
+    engineOptions: {}
   }
 
   const stats = await compiler('data/source.ejs', options)
@@ -19,7 +20,7 @@ test('Renders an Ejs template', async () => {
   expect(source).toContain('<h2>A template rendered by Ejs</h2>')
 })
 
-test('Renders an Ejs template with partial', async () => {
+test('Renders an Ejs template with partial (views)', async () => {
   const options = {
     engine: 'ejs',
     locals: {
@@ -27,9 +28,27 @@ test('Renders an Ejs template with partial', async () => {
       desc: 'A template rendered by Ejs'
     },
     engineOptions: {
-      views: [
-        path.resolve(__dirname, 'data')
-      ]
+      views: [ path.resolve(__dirname, 'data') ]
+    }
+  }
+
+  const stats = await compiler('data/source-partial.ejs', options)
+  const output = stats.toJson().modules[0].source
+  const source = helpers.value(output)
+
+  expect(source).toContain('<h1>Ejs Template</h1>')
+  expect(source).toContain('<h2>A template rendered by Ejs</h2>')
+})
+
+test('Renders an Ejs template with partial (filename)', async () => {
+  const options = {
+    engine: 'ejs',
+    locals: {
+      title: 'Ejs Template',
+      desc: 'A template rendered by Ejs'
+    },
+    engineOptions: function (info) {
+      return { filename: info.filename }
     }
   }
 

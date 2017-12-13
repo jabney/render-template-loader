61a42ea73bdc8c0a3ecfb974e2c56ac08ee9a788
James Abney <james.abney@gmail.com>
2017-12-12 22:17:23 -0800

    Add partials test

--- test/jade.spec.js
+++ test/jade.spec.js
@@ -17,3 +17,23 @@ test('Renders a Jade template', async () => {
   expect(source).toContain('<h1>Jade Template</h1>')
   expect(source).toContain('<h2>A template rendered by Jade</h2>')
 })
+
+test('Renders a Jade template with partial', async () => {
+  const options = {
+    engine: 'jade',
+    locals: {
+      title: 'Jade Template',
+      desc: 'A template rendered by Jade'
+    },
+    engineOptions: function (info) {
+      return { filename: info.filename }
+    }
+  }
+
+  const stats = await compiler('data/source-partial.jade', options)
+  const output = stats.toJson().modules[0].source
+  const source = helpers.value(output)
+
+  expect(source).toContain('<h1>Jade Template</h1>')
+  expect(source).toContain('<h2>A template rendered by Jade</h2>')
+})

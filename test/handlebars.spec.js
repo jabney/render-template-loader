300939ed3c315237fab1072b673bd818b9cda3d3
James Abney <james.abney@gmail.com>
2017-12-13 21:40:26 -0800

    Add handlebars partials spec

--- test/handlebars.spec.js
+++ test/handlebars.spec.js
@@ -17,3 +17,23 @@ test('Renders a Handlebars template', async () => {
   expect(source).toContain('<h1>Handlebars Template</h1>')
   expect(source).toContain('<h2>A template rendered by Handlebars</h2>')
 })
+
+test('Renders a Handlebars template with partial (init)', async () => {
+  const options = {
+    engine: 'handlebars',
+    init: function (engine, info) {
+      engine.registerPartial('description', '<h2>{{ desc }}</h2>')
+    },
+    locals: {
+      title: 'Handlebars Template',
+      desc: 'A template rendered by Handlebars'
+    }
+  }
+
+  const stats = await compiler('data/source-partial.hbs', options)
+  const output = stats.toJson().modules[0].source
+  const source = helpers.value(output)
+
+  expect(source).toContain('<h1>Handlebars Template</h1>')
+  expect(source).toContain('<h2>A template rendered by Handlebars</h2>')
+})
